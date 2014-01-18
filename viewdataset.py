b660679cf29c4f2d87fd43dbe08575325a8e614d
Andrew Miller <amiller@dappervision.com>
2014-01-18 18:08:23 -0500

    Added demo 3d viewer
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- viewdataset.py
+++ viewdataset.py
@@ -7,11 +7,57 @@ import subprocess
 import dataset
 import colormap
 import pylab
+from wxpy3d import PointWindow
+from OpenGL.GL import *
+from rtmodel import pointmodel
+from rtmodel.rangeimage import RangeImage
+from rtmodel.camera import kinect_camera
+cam = kinect_camera()
+
+if not 'window' in globals():
+    window = PointWindow(size=(640,480))
+    #window.Move((632,100))
+    print """
+    Demo Objrender:
+        search for "Points to draw" and uncomment different points
+        refresh()
+        perturb(): apply a random matrix to the point model
+        load_obj(): select a random object and load it
+    """
+
+@window.event
+def post_draw():
+    global points
+    if not 'points' in globals(): return
+
+    # Perturbed points (previous estimate)
+    glColor(0,1,0)
+    glPointSize(1)
+    points.draw()
+
+window.Refresh()
 
 def show_depth(name, depth):
     im = cv.CreateImage((depth.shape[1],depth.shape[0]), 8, 3)
     cv.SetData(im, colormap.color_map(depth/2))
     cv.ShowImage(name, im)
+
+def once():
+    # 2D colormap preview
+    dataset.advance()
+    show_depth("depth_0", dataset.depths[0])
+
+    # 3D point cloud view
+    depth = dataset.depths[0]
+    rimg = RangeImage(depth, cam)
+    rimg.compute_points()
+    global points
+    points = rimg.point_model()
+    window.lookat = points.RT[:3,3] + points.xyz[:,:3].mean(0)
+    window.Refresh()
+
+    pylab.waitforbuttonpress(0.05)
+    
 
 def go(dset=None):
     if dset is None:
@@ -20,6 +66,4 @@ def go(dset=None):
         dataset.load_dataset(dset)
 
     while True:
-        dataset.advance()
-        show_depth("depth_0", dataset.depths[0])
-        pylab.waitforbuttonpress(0.05)
+        once()
