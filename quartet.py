a93a48557901f778c9bce1ceed7af9d79347f8a5
Andrew Miller <amiller@dappervision.com>
2014-02-13 00:23:16 -0500

    config
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

---
+++ quartet.py
@@ -0,0 +1,35 @@
+import matplotlib
+matplotlib.use('WX')
+import record
+import viewdataset
+import signal
+import sys
+import argparse
+from config import NUM_KINECTS, KINECT_PATH
+
+def main():
+    parser = argparse.ArgumentParser(description='Kinect Data Saver')
+    subparsers = parser.add_subparsers()
+    subparser = subparsers.add_parser('display')
+    subparser.set_defaults(mode='display')
+    subparser = subparsers.add_parser('save')
+    subparser.set_defaults(mode='save')
+    subparser = subparsers.add_parser('playback')
+    subparser.set_defaults(mode='playback')
+    subparser = subparsers.add_parser('playback3d')
+    subparser.set_defaults(mode='playback3d')
+    args = vars(parser.parse_args())    
+    mode = args['mode']
+    if mode == 'display':
+        while 1:
+            record.preview(range(NUM_KINECTS))
+    elif mode == 'playback':
+        viewdataset.go(path=KINECT_PATH)
+    elif mode == 'playback3d':
+        viewdataset.go(path=KINECT_PATH, threed=True)
+    elif mode == 'save':
+        record.record(cams=range(NUM_KINECTS), do_rgb=True)
+
+if __name__ == '__main__':
+    main()
+
