1bf3e0072952c36e72b1abaaa567ebbf4c8c9465
Andrew Miller <amiller@dappervision.com>
2014-02-13 19:32:17 -0500

    bgr imshow
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- view2d.py
+++ view2d.py
@@ -8,7 +8,7 @@ import dataset
 from config import KINECT_PATH
 
 def show_rgb(name, image):
-    cv2.imshow(name, image)
+    cv2.imshow(name, image[:,:,::-1])
 
 def show_depth(name, depth):
     cv2.imshow(name, 1024./depth)
