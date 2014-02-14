78f4ae7e766a39cb690e81f7f50809db62ada5f3
Andrew Miller <amiller@dappervision.com>
2014-02-14 10:25:13 -0500

    now with snappy
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- viewfakenect.py
+++ viewfakenect.py
@@ -2,19 +2,20 @@ import glob
 import cv2
 import numpy as np
 import os
+import snappy
 
 def view(filename):
     cv2.namedWindow('rgb')
     cv2.namedWindow('depth')
     cv2.moveWindow('rgb',640,0)
     cv2.moveWindow('depth',0,0)
-    files = glob.glob('%s/*.pgm' % (filename,)) + glob.glob('%s/*.ppm' % (filename,))
+    files = glob.glob('%s/*.snappy' % (filename,)) + glob.glob('%s/*.jpg' % (filename,))
     files = sorted(files, key=lambda f: os.path.basename(f)[2:])
     for f in files:
-        if f.endswith('.ppm'):
+        if f.endswith('.jpg'):
             cv2.imshow('rgb', cv2.imread(f))
             cv2.waitKey(20)
-        if f.endswith('.pgm'):
-            depth = np.fromstring(open(f).read()[17:], dtype='uint16').reshape((480,640))
+        if f.endswith('.snappy'):
+            depth = np.fromstring(snappy.decompress(open(fn).read()), dtype='uint16').reshape((480,640))
             cv2.imshow('depth', 1024./depth)
             cv2.waitKey(20)
