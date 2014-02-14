7fa31e37f5a937410ed5f964f5ebc07e5573e50a
Andrew Miller <amiller@dappervision.com>
2014-02-14 10:42:17 -0500

    dump to snappy
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- viewfakenect.py
+++ viewfakenect.py
@@ -9,13 +9,13 @@ def view(filename):
     cv2.namedWindow('depth')
     cv2.moveWindow('rgb',640,0)
     cv2.moveWindow('depth',0,0)
-    files = glob.glob('%s/*.dump' % (filename,)) + glob.glob('%s/*.jpg' % (filename,))
+    files = glob.glob('%s/*.snappy' % (filename,)) + glob.glob('%s/*.jpg' % (filename,))
     files = sorted(files, key=lambda f: os.path.basename(f)[2:])
     for f in files:
         if f.endswith('.jpg'):
             cv2.imshow('rgb', cv2.imread(f))
             cv2.waitKey(20)
-        if f.endswith('.dump'):
+        if f.endswith('.snappy'):
             depth = np.fromstring(snappy.decompress(open(f).read()), dtype='uint16').reshape((480,640))
             cv2.imshow('depth', 1024./depth)
             cv2.waitKey(20)
