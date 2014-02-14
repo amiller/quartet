0ec88d08ec279b50b5f26c342b8c9c9523449ca1
Andrew Miller <amiller@dappervision.com>
2014-02-13 21:48:23 -0500

    sleep
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- view2d.py
+++ view2d.py
@@ -32,10 +32,10 @@ def preview(cams):
     for cam in cams:
         (depth,_) = opennpy.sync_get_depth(cam)
         show_depth('depth_%d'%cam, depth)
-        cv2.waitKey(5)
+        cv2.waitKey(100)
         (rgb,_) = opennpy.sync_get_video(cam)
         show_rgb('rgb_%d'%cam, rgb)
-        cv2.waitKey(5)
+        cv2.waitKey(100)
 
 def go(dset=None, path=KINECT_PATH, threed=False):
     if dset is None:
