372a91d3fd1790a0754a9021e984c2cade956281
Andrew Miller <amiller@dappervision.com>
2014-02-14 04:05:59 -0500

    current fabric file
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,20 @@
 from fabric.api import run, cd, sudo, put
+from fabric.contrib.files import append
 
 def dmidecode():
     sudo('dmidecode -s system-product-name')
 
 def save():
     with cd('quartet_amiller'):
-        run('python quartet.py save')
+        run('DISPLAY=:0 xhost +x localhost')
+        run('DISPLAY=:0 python quartet.py save')
 
 def kill():
     sudo('killall -9 XnSensorServer')
     sudo('killall -9 python')
 
-def renice():
-    sudo('renice +20 $(pidof XnSensorServer)')
-    sudo('renice -20 $(pidof python)')
+def reboot():
+    sudo('reboot')
 
 def update():
     with cd('quartet_amiller'):
@@ -22,6 +23,11 @@ def update():
         run('git submodule update')
         with cd('opennpy'):
             sudo('python setup.py install')
+
+def playback():
+    with cd('quartet_amiller'):
+        run('DISPLAY=:0 xhost +x localhost')
+        run('DISPLAY=:0 python quartet.py playback')
 
 def inspect():
     with cd('quartet_amiller'):
@@ -41,20 +47,28 @@ def niview():
         run('DISPLAY=:0 ./NiViewer')
 
 def setup():
-    sudo('apt-get update')
-    sudo('apt-get install -y libusb-1.0-0-dev freeglut3-dev doxygen cython ipython python-scipy python-numpy python-opencv python-wxgtk2.8 git default-jdk build-essential g++ cmake emacs23-nox python-wxgtk2.8 python-opengl git python-matplotlib python-snappy python-zmq')
-    run('git clone https://github.com/amiller/quartet quartet_amiller')
+    #sudo('apt-get update')
+    sudo('apt-get install -y libusb-1.0-0-dev freeglut3-dev libxi-dev libxmu-dev doxygen cython ipython python-scipy python-numpy python-opencv python-wxgtk2.8 git default-jdk build-essential g++ cmake emacs23-nox python-wxgtk2.8 python-opengl git python-matplotlib python-snappy python-zmq')
+    append('~/.bashrc', 'DISPLAY=:0 xhost +x localhost')
+    append('~/.bashrc', 'export DISPLAY=:0')
+    #run('git clone https://github.com/amiller/quartet quartet_amiller')
     with cd('quartet_amiller'):
         run('git submodule init')
         run('git submodule update')
-        with cd('OpenNI/Platform/Linux/CreateRedist'):
-            run('./RedistMaker')
-            with cd('../Redist/OpenNI-Bin-Dev-Linux-x64-v1.5.7.10'):
-                sudo('bash install.sh')
-        with cd('SensorKinect/Platform/Linux/CreateRedist'):
-            run('./RedistMaker')
-            with cd('../Redist/Sensor-Bin-Linux-x64-v5.1.2.1/'):
-                sudo('bash install.sh')
+        with cd('libfreenect'):
+            run('mkdir -p build')
+            with cd('build'):
+                run('cmake -DBUILD_PYTHON=on -DLIB_SUFFIX= -DCMAKE_INSTALL_PREFIX=/usr ..')
+                run('make')
+                sudo('make install')
+        # with cd('OpenNI/Platform/Linux/CreateRedist'):
+        #     run('./RedistMaker')
+        #     with cd('../Redist/OpenNI-Bin-Dev-Linux-x64-v1.5.7.10'):
+        #         sudo('bash install.sh')
+        # with cd('SensorKinect/Platform/Linux/CreateRedist'):
+        #     run('./RedistMaker')
+        #     with cd('../Redist/Sensor-Bin-Linux-x64-v5.1.2.1/'):
+        #         sudo('bash install.sh')
         with cd('opennpy'):
             sudo('python setup.py install')
         with cd('wxpy3d'):
