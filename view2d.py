f53a7e9c083f6e68dfd65f9fb4e839b1b91d16e0
Andrew Miller <amiller@dappervision.com>
2014-02-13 19:02:33 -0500

    view2d
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- view2d.py
+++ view2d.py
@@ -30,7 +30,6 @@ def preview(cams):
     for cam in cams:
         (depth,_) = opennpy.sync_get_depth(cam)
         print(depth.shape)
-        print "Depth aligned:", is_aligned(depth)
         depth_cache.append(np.array(depth))
         depth_cache = depth_cache[-6:]
         show_depth('depth_%d'%cam, depth)
