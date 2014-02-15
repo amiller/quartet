3ce482fce76a977e3b12edbf039017aaf9e05271
Andrew Miller <amiller@dappervision.com>
2014-02-15 01:01:05 -0500

    host-
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- fabfile.py
+++ fabfile.py
@@ -26,10 +26,16 @@ def reboot():
 def trim():
     sudo('fstrim -v /')
 
+def ntp():
+    sudo('service ntp stop')
+    sudo('ntpdate -s time.nist.gov')
+    sudo('service ntp start')
+    sudo('ntpq -p')
+
 def mostrecent():
     result = run('ls -1 quartet_amiller/data/kinect_sets | tail -n1')
     get('quartet_amiller/data/kinect_sets/%s' % (result,), 
-        'data/quartetdata/%s/%%(host)s' % (label))
+        'data/quartetdata/%s/host-%%(host)s' % (label))
 
 def update():
     with cd('quartet_amiller'):
@@ -63,7 +69,7 @@ def display():
 
 def setup():
     sudo('apt-get update')
-    sudo('apt-get install -y ocl-icd-libopencl1 libusb-1.0-0-dev freeglut3-dev libxi-dev libxmu-dev doxygen cython ipython python-scipy python-numpy python-opencv libopencv-dev python-wxgtk2.8 git default-jdk build-essential g++ cmake emacs23-nox python-wxgtk2.8 python-opengl git python-matplotlib python-snappy libsnappy-dev python-zmq indicator-cpufreq')
+    sudo('apt-get install -y ocl-icd-libopencl1 libusb-1.0-0-dev freeglut3-dev libxi-dev libxmu-dev doxygen cython ipython python-scipy python-numpy python-opencv libopencv-dev python-wxgtk2.8 git default-jdk build-essential g++ cmake emacs23-nox python-wxgtk2.8 python-opengl git python-matplotlib python-snappy libsnappy-dev python-zmq indicator-cpufreq ntp')
     #append('~/.bashrc', 'DISPLAY=:0 xhost +x localhost')
     #append('~/.bashrc', 'export DISPLAY=:0')
     run('if ! [ -a quartet_amiller ]; then git clone https://github.com/amiller/quartet quartet_amiller; fi')
@@ -76,16 +82,16 @@ def setup():
                 run('cmake -DBUILD_PYTHON=on -DLIB_SUFFIX= -DCMAKE_INSTALL_PREFIX=/usr ..')
                 run('make')
                 sudo('make install')
-        with cd('OpenNI/Platform/Linux/CreateRedist'):
-            run('./RedistMaker')
-            with cd('../Redist/OpenNI-Bin-Dev-Linux-x64-v1.5.7.10'):
-                sudo('bash install.sh')
-        with cd('SensorKinect/Platform/Linux/CreateRedist'):
-            run('./RedistMaker')
-            with cd('../Redist/Sensor-Bin-Linux-x64-v5.1.2.1/'):
-                sudo('bash install.sh')
-        with cd('opennpy'):
-            sudo('python setup.py install')
+        # with cd('OpenNI/Platform/Linux/CreateRedist'):
+        #     run('./RedistMaker')
+        #     with cd('../Redist/OpenNI-Bin-Dev-Linux-x64-v1.5.7.10'):
+        #         sudo('bash install.sh')
+        # with cd('SensorKinect/Platform/Linux/CreateRedist'):
+        #     run('./RedistMaker')
+        #     with cd('../Redist/Sensor-Bin-Linux-x64-v5.1.2.1/'):
+        #         sudo('bash install.sh')
+        # with cd('opennpy'):
+        #     sudo('python setup.py install')
         with cd('wxpy3d'):
             sudo('python setup.py install')
         with cd('rtmodel'):
