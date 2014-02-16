0539d5ad0a6aeef561e6db10f4e96ce9cad32215
Brandyn A. White <bwhite@dappervision.com>
2014-02-15 22:09:58 -0500

    Skip directories that have no data
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- inspect_cams.py
+++ inspect_cams.py
@@ -4,9 +4,6 @@ import os
 import time
 import re
 
-kinect_dir = sorted(glob.glob(config.KINECT_PATH + '/*'))[-1]
-eye_dir = sorted(glob.glob(config.EYE_PATH + '/*'))[-1]
-
 def check_dir(name, d, extension, fn_re, recent_time=30.):
     data = [float(fn_re.search(fn).groups()[0])
             for fn in sorted(glob.glob(d + "/*" + extension))]
@@ -20,6 +17,18 @@ def check_dir(name, d, extension, fn_re, recent_time=30.):
     fps = len(data) / float(duration)
     fps_recent = counts_recent / min(float(duration), recent_time)
     print('[%s][%s] Last (sec): %.2f Dur (sec): %.1f Cam: %.2f CamRecent: %.2f Fps: %.2f FpsRecent: %.2f' % (name, extension, (time.time() - data[-1]), duration, len(data), counts_recent, fps, fps_recent))
-check_dir('eye', eye_dir, '.jpg', re.compile('.+/.+_(.+)_.+_.+\..+'))
-check_dir('kinect', kinect_dir, '.snappy', re.compile('.+/.+-(.+)-.+\..+'))
-check_dir('kinect', kinect_dir, '.jpg', re.compile('.+/.+-(.+)-.+\..+'))
+
+try:
+    eye_dir = sorted(glob.glob(config.EYE_PATH + '/*'))[-1]
+except IndexError:
+    pass
+else:
+    check_dir('eye', eye_dir, '.jpg', re.compile('.+/.+_(.+)_.+_.+\..+'))
+
+try:
+    kinect_dir = sorted(glob.glob(config.KINECT_PATH + '/*'))[-1]
+except IndexError:
+    pass
+else:
+    check_dir('kinect', kinect_dir, '.snappy', re.compile('.+/.+-(.+)-.+\..+'))
+    check_dir('kinect', kinect_dir, '.jpg', re.compile('.+/.+-(.+)-.+\..+'))
